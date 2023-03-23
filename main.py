a2ca542c0d6e8b911d0e8a4e477a76af1d1ebd9b
Bikash <60249539+dashbikash@users.noreply.github.com>
2023-03-23 20:27:17 +0000

    Initial commit

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    logging.debug(Request.headers)
-    return "Hello Vidura"
+    logging.debug(Request().headers)
+    return "Hello Vidura."
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
